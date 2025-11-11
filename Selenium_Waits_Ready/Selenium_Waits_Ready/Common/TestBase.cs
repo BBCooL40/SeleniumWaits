ec7db0f05bc57798e6f9a685617f9a27497a6bee
Anton Tzonev <anton.tzonev@yahoo.com>
2025-11-11 11:59:42 +0200

    Update CI workflow (install chromium & headless mode)

--- Selenium_Waits_Ready/Selenium_Waits_Ready/Common/TestBase.cs
+++ Selenium_Waits_Ready/Selenium_Waits_Ready/Common/TestBase.cs
@@ -1,30 +1,35 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using WebDriverManager;
-using WebDriverManager.DriverConfigs.Impl;
-using System;
 
-namespace Selenium_Waits_Ready.Common
+public class TestBase
 {
-    public class TestBase
+    protected IWebDriver driver;
+
+    [SetUp]
+    public void Setup()
     {
-        protected IWebDriver Driver;
+        var options = new ChromeOptions();
 
-        [SetUp]
-        public void Setup()
-        {
-            new DriverManager().SetUpDriver(new ChromeConfig());
-            var options = new ChromeOptions();
-            options.AddArgument("--start-maximized");
-            Driver = new ChromeDriver(options);
-            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-        }
+        // Headless & CI-friendly args
+        options.AddArgument("--headless=new"); // ??? "--headless=chrome" ?? ??-????? ??????
+        options.AddArgument("--no-sandbox");
+        options.AddArgument("--disable-dev-shm-usage");
+        options.AddArgument("--disable-gpu");
+        options.AddArgument("--window-size=1920,1080");
+        options.AddArgument("--disable-extensions");
+        options.AddArgument("--disable-infobars");
 
-        [TearDown]
-        public void TearDown()
-        {
-            try { Driver?.Quit(); Driver?.Dispose(); } catch { }
-        }
+        // ??? chromedriver/Chromium ?? ?? ???????????? ???, ????? ?? ?????? BinaryLocation:
+        // options.BinaryLocation = "/usr/bin/chromium-browser";
+
+        driver = new ChromeDriver(options);
+        driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver?.Quit();
     }
 }
